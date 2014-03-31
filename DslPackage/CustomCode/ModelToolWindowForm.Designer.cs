bdc8f95361e74304a1e4c6c1c3693175e5adf681
Jun Wang <jun.v.wang@hotmail.com>
2014-03-31 21:56:46 +0800

    model window layout

--- DslPackage/CustomCode/ModelToolWindowForm.Designer.cs
+++ DslPackage/CustomCode/ModelToolWindowForm.Designer.cs
@@ -29,85 +29,148 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.label1 = new System.Windows.Forms.Label();
-            this.treeListView1 = new BrightIdeasSoftware.TreeListView();
-            this.olvColumn1 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
-            this.olvColumn2 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
-            this.olvColumn3 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
-            this.olvColumn4 = ((BrightIdeasSoftware.OLVColumn)(new BrightIdeasSoftware.OLVColumn()));
-            ((System.ComponentModel.ISupportInitialize)(this.treeListView1)).BeginInit();
+            this.tlpViewModelLayout = new System.Windows.Forms.TableLayoutPanel();
+            this.tlvViewModel = new BrightIdeasSoftware.DataTreeListView();
+            this.tlpWidgetModelLayout = new System.Windows.Forms.TableLayoutPanel();
+            this.tlpActionModelLayout = new System.Windows.Forms.TableLayoutPanel();
+            this.tlvWidgetModel = new BrightIdeasSoftware.DataTreeListView();
+            this.tlvActionModel = new BrightIdeasSoftware.DataTreeListView();
+            this.tlpViewModelLayout.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tlvViewModel)).BeginInit();
+            this.tlpWidgetModelLayout.SuspendLayout();
+            this.tlpActionModelLayout.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tlvWidgetModel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tlvActionModel)).BeginInit();
             this.SuspendLayout();
             // 
-            // label1
+            // tlpViewModelLayout
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 13);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(97, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "To be implemented";
+            this.tlpViewModelLayout.ColumnCount = 4;
+            this.tlpViewModelLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 120F));
+            this.tlpViewModelLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlpViewModelLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tlpViewModelLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tlpViewModelLayout.Controls.Add(this.tlvViewModel, 0, 1);
+            this.tlpViewModelLayout.Location = new System.Drawing.Point(12, 12);
+            this.tlpViewModelLayout.Name = "tlpViewModelLayout";
+            this.tlpViewModelLayout.RowCount = 3;
+            this.tlpViewModelLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tlpViewModelLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlpViewModelLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 21F));
+            this.tlpViewModelLayout.Size = new System.Drawing.Size(735, 179);
+            this.tlpViewModelLayout.TabIndex = 0;
             // 
-            // treeListView1
+            // tlvViewModel
             // 
-            this.treeListView1.AllColumns.Add(this.olvColumn1);
-            this.treeListView1.AllColumns.Add(this.olvColumn2);
-            this.treeListView1.AllColumns.Add(this.olvColumn3);
-            this.treeListView1.AllColumns.Add(this.olvColumn4);
-            this.treeListView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.olvColumn1,
-            this.olvColumn2,
-            this.olvColumn3,
-            this.olvColumn4});
-            this.treeListView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.treeListView1.Location = new System.Drawing.Point(0, 0);
-            this.treeListView1.Name = "treeListView1";
-            this.treeListView1.OwnerDraw = true;
-            this.treeListView1.ShowGroups = false;
-            this.treeListView1.Size = new System.Drawing.Size(284, 262);
-            this.treeListView1.TabIndex = 1;
-            this.treeListView1.UseCompatibleStateImageBehavior = false;
-            this.treeListView1.View = System.Windows.Forms.View.Details;
-            this.treeListView1.VirtualMode = true;
+            this.tlpViewModelLayout.SetColumnSpan(this.tlvViewModel, 4);
+            this.tlvViewModel.DataSource = null;
+            this.tlvViewModel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlvViewModel.Location = new System.Drawing.Point(3, 23);
+            this.tlvViewModel.Name = "tlvViewModel";
+            this.tlvViewModel.OwnerDraw = true;
+            this.tlvViewModel.RootKeyValueString = "";
+            this.tlvViewModel.ShowGroups = false;
+            this.tlvViewModel.Size = new System.Drawing.Size(729, 132);
+            this.tlvViewModel.TabIndex = 0;
+            this.tlvViewModel.UseCompatibleStateImageBehavior = false;
+            this.tlvViewModel.View = System.Windows.Forms.View.Details;
+            this.tlvViewModel.VirtualMode = true;
             // 
-            // olvColumn1
+            // tlpWidgetModelLayout
             // 
-            this.olvColumn1.CellPadding = null;
+            this.tlpWidgetModelLayout.ColumnCount = 4;
+            this.tlpWidgetModelLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 120F));
+            this.tlpWidgetModelLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlpWidgetModelLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tlpWidgetModelLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tlpWidgetModelLayout.Controls.Add(this.tlvWidgetModel, 0, 1);
+            this.tlpWidgetModelLayout.Location = new System.Drawing.Point(9, 197);
+            this.tlpWidgetModelLayout.Name = "tlpWidgetModelLayout";
+            this.tlpWidgetModelLayout.RowCount = 3;
+            this.tlpWidgetModelLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tlpWidgetModelLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlpWidgetModelLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tlpWidgetModelLayout.Size = new System.Drawing.Size(738, 153);
+            this.tlpWidgetModelLayout.TabIndex = 1;
             // 
-            // olvColumn2
+            // tlpActionModelLayout
             // 
-            this.olvColumn2.CellPadding = null;
+            this.tlpActionModelLayout.ColumnCount = 4;
+            this.tlpActionModelLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 120F));
+            this.tlpActionModelLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlpActionModelLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tlpActionModelLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tlpActionModelLayout.Controls.Add(this.tlvActionModel, 0, 1);
+            this.tlpActionModelLayout.Location = new System.Drawing.Point(12, 385);
+            this.tlpActionModelLayout.Name = "tlpActionModelLayout";
+            this.tlpActionModelLayout.RowCount = 3;
+            this.tlpActionModelLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tlpActionModelLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlpActionModelLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tlpActionModelLayout.Size = new System.Drawing.Size(735, 139);
+            this.tlpActionModelLayout.TabIndex = 2;
             // 
-            // olvColumn3
+            // tlvWidgetModel
             // 
-            this.olvColumn3.CellPadding = null;
+            this.tlpWidgetModelLayout.SetColumnSpan(this.tlvWidgetModel, 4);
+            this.tlvWidgetModel.DataSource = null;
+            this.tlvWidgetModel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlvWidgetModel.Location = new System.Drawing.Point(3, 23);
+            this.tlvWidgetModel.Name = "tlvWidgetModel";
+            this.tlvWidgetModel.OwnerDraw = true;
+            this.tlvWidgetModel.RootKeyValueString = "";
+            this.tlvWidgetModel.ShowGroups = false;
+            this.tlvWidgetModel.Size = new System.Drawing.Size(732, 107);
+            this.tlvWidgetModel.TabIndex = 0;
+            this.tlvWidgetModel.UseCompatibleStateImageBehavior = false;
+            this.tlvWidgetModel.View = System.Windows.Forms.View.Details;
+            this.tlvWidgetModel.VirtualMode = true;
             // 
-            // olvColumn4
+            // tlvActionModel
             // 
-            this.olvColumn4.CellPadding = null;
+            this.tlpActionModelLayout.SetColumnSpan(this.tlvActionModel, 4);
+            this.tlvActionModel.DataSource = null;
+            this.tlvActionModel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlvActionModel.Location = new System.Drawing.Point(3, 23);
+            this.tlvActionModel.Name = "tlvActionModel";
+            this.tlvActionModel.OwnerDraw = true;
+            this.tlvActionModel.RootKeyValueString = "";
+            this.tlvActionModel.ShowGroups = false;
+            this.tlvActionModel.Size = new System.Drawing.Size(729, 93);
+            this.tlvActionModel.TabIndex = 0;
+            this.tlvActionModel.UseCompatibleStateImageBehavior = false;
+            this.tlvActionModel.View = System.Windows.Forms.View.Details;
+            this.tlvActionModel.VirtualMode = true;
             // 
             // ModelToolWindowForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
-            this.Controls.Add(this.treeListView1);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(759, 536);
+            this.Controls.Add(this.tlpActionModelLayout);
+            this.Controls.Add(this.tlpWidgetModelLayout);
+            this.Controls.Add(this.tlpViewModelLayout);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "ModelToolWindowForm";
             this.Text = "ModelToolWindowForm";
-            ((System.ComponentModel.ISupportInitialize)(this.treeListView1)).EndInit();
+            this.tlpViewModelLayout.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.tlvViewModel)).EndInit();
+            this.tlpWidgetModelLayout.ResumeLayout(false);
+            this.tlpActionModelLayout.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.tlvWidgetModel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tlvActionModel)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private BrightIdeasSoftware.TreeListView treeListView1;
-        private BrightIdeasSoftware.OLVColumn olvColumn1;
-        private BrightIdeasSoftware.OLVColumn olvColumn2;
-        private BrightIdeasSoftware.OLVColumn olvColumn3;
-        private BrightIdeasSoftware.OLVColumn olvColumn4;
+        private System.Windows.Forms.TableLayoutPanel tlpViewModelLayout;
+        private BrightIdeasSoftware.DataTreeListView tlvViewModel;
+        private System.Windows.Forms.TableLayoutPanel tlpWidgetModelLayout;
+        private BrightIdeasSoftware.DataTreeListView tlvWidgetModel;
+        private System.Windows.Forms.TableLayoutPanel tlpActionModelLayout;
+        private BrightIdeasSoftware.DataTreeListView tlvActionModel;
+
     }
 }
